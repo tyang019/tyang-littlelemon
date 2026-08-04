6eccfad9477d1faeb4ff05aebc728516d19eee84
tyang019 <tyang019@fiu.edu>
2026-08-04 00:42:20 -0500

    Re-modify Account registration and login form

--- src/User.jsx
+++ src/User.jsx
@@ -27,69 +27,85 @@ const [registerData, setRegisterData] = useState({
     <section>
       <article className="message-form">
         <form onSubmit={handleSubmit} >
-          <h1>Account Sign In</h1>
-        <label htmlFor="email">Email</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
-          value={loginData.email} 
-          required
-          onChange={handleChange} 
-        />
-
-        <label htmlFor="password">Password</label>
-        <input 
-          type="password" 
-          id="password" 
-          name="password" 
-          value={loginData.password} 
-          required
-          onChange={handleChange} 
-        />
-
+          <h1 style={{
+            width: "100%",
+            height: "100px",
+            textAlign: "center",
+            marginBottom: "1rem",
+            margin: "1rem 0",
+            padding: "1rem",
+          }}>Account Sign In</h1>
+          <article className="label-input">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              value={loginData.email} 
+              required
+              onChange={handleChange} 
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              value={loginData.password} 
+              required
+              onChange={handleChange} 
+            />
+          </article>
         <button type="submit">Sign In</button>
         </form>
-
-        <form> 
-          <h1>Register</h1>
-          <label>Full Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={registerData.name}
-            required
-            onChange={handleChange}
-          ></input>
-          <label>Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={registerData.email}
-            required
-            onChange={handleChange}
-          ></input>
-          <label>Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={registerData.password}
-            required
-            onChange={handleChange}
-          ></input>
-          <label>Phone</label>
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            value={registerData.phone}
-            required
-            onChange={handleChange}
-          ></input>
-          <button type="submit">Register</button>
+    
+        <form>
+          <h1 style={{
+            width: "100%",
+            height: "100px",
+            textAlign: "center",
+            marginBottom: "1rem",
+            margin: "1rem 0",
+            padding: "1rem",
+          }}>Register</h1>
+          <article className="label-input">
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={registerData.name}
+              required
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={registerData.email}
+              required
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={registerData.password}
+              required
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              value={registerData.phone}
+              required
+              onChange={handleChange}
+            ></input>
+          </article>
+          <button type="submit">Register</button>          
         </form>
       </article>
     </section>
